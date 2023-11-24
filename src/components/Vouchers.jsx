1141e13d8256192997d23237e26bcd66b2b8715e
mehjbeen22 <mehjbeen22@navgurukul.org>
2023-11-24 11:50:12 +0530

    checkbox

--- src/components/Vouchers.jsx
+++ src/components/Vouchers.jsx
@@ -35,8 +35,23 @@ const Vouchers = () => {
               <div className={styles.OuterBox} key={index}>
                 <div className={styles.middleBox}>
                   <div className={styles.shapes}>
-                   
-                    <div>Shapes</div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="icon icon-tabler icon-tabler-checkbox"
+                      width="30"
+                      height="30"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M9 11l3 3l8 -8" />
+                      <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+                    </svg>
+                    <div style={{ height: "10rem" }}></div>
                   </div>
                 </div>
                 <p className={styles.title}>
